1a98b80689833db3cdb592922e54cd6da3942f93
Sheikh Sayed <sksayed@live.com>
2019-09-16 22:15:59 +0600

    title component e star er kaj kora hoise

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  StarState:boolean = false ;
+
+  ClickOnStar()
+  {
+    this.StarState = !this.StarState;
+  }
+
   constructor() { }
 
   ngOnInit() {
